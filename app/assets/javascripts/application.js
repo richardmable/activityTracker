6e9b3a60b3a2d0bee5dad26664be9941a397ecd3
Richard Mable <richardbmable@gmail.com>
2016-02-22 15:37:51 -0500

    added dependencises in the application.js file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,4 +2,6 @@
 //= require AngularDevise
 //= require angular-rails-templates
 //= require angular-ui-router
+//= require ng-token-auth
+//= require angular-cookie
 //= require_tree .
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+  root 'application#angular'
   mount_devise_token_auth_for 'User', at: 'auth'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
