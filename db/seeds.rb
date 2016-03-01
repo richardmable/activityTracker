4fb905a3d0cd883f71a37bd7f5ce1b9e119d628c
Richard Mable <richardbmable@gmail.com>
2016-03-01 15:53:20 -0500

    added some motions to the seed file associated with user_id 1

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Motion.create([
+{user_id: 1, steps: 8637, date: "2016-02-18"},
+{user_id: 1, steps: 10072, date: "2016-02-19"},
+{user_id: 1, steps: 7293, date: "2016-02-20"},
+{user_id: 1, steps: 14722, date: "2016-02-22"}
+
+])
