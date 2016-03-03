398d0824c939f4a8f21342de899ffa59c19112a7
Richard Mable <richardbmable@gmail.com>
2016-03-02 23:52:50 -0500

    about to mess with ng-token-auth.js config file

--- app/assets/javascripts/nav/navCtrl.js
+++ app/assets/javascripts/nav/navCtrl.js
@@ -5,32 +5,10 @@ angular.module('activityTracker')
 	'$scope',
 	'$state',
 	function($scope, $state){
+		// function to log user out
 		$scope.handleSignOutBtnClick = function(){
 			$auth.signOut();
-			console.log("logged out");
+			$state.go('home');
 		}
-
-
-	// 	// this exposes the isAuthenticated and logout method to $scope
-	// 	$scope.signedIn = Auth.isAuthenticated;
-	// 	$scope.logout = Auth.logout;
-	// 	// this attempts to access Auth.currentUser when the controller loads to set $scope.user
-	// 	// only completes if there is a user logged in
-	// 	Auth.currentUser().then(function(user){
-	// 		$scope.user = user;
-	// 	});
-	// 	// event listeners to handle when the user authenticates and logs out
-	// 	// angular-devise automatically broadcasts events when authentication happens
-	// 	// set $scope.user on new user registration
-		
-	// 	});
-	// 	// set $scope.user on user login
-	// 	$scope.$on('devise:login', function(e, user){
-	// 		$scope.user = user;
-	// 	});
-	// 	// clear $scope.user on user log out
-	// 	$scope.$on('devise:logout', function(e, user){
-	// 		$scope.user = {};
-	// 	})
 
 }]);
